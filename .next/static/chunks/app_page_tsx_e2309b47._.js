974505b56d8b577c6ba804bfca8aecbccbd276ee
Rayane Achouchi <rayaneachouchi25@outlook.fr>
2025-05-29 13:38:32 +0200

    Modified Quizz to be a dialog modal that pop up | Changed website font

--- .next/static/chunks/app_page_tsx_e2309b47._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_e132cd3e._.js",
-  "static/chunks/components_HomeSections_2f1be139._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/components_ShipMap_tsx_999c79ee._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_3960a2e9._.js",
-  "static/chunks/components_ShipMap_tsx_5036910d._.js"
-],
-    source: "dynamic"
-});
